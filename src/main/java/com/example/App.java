190421e6a54216c909ce797c56aea3c26176bfa0
yasasvini2818 <yasasvini0728@gmail.com>
2025-04-29 12:21:28 +0530

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=10;
+        int b=6;
+        int c=a+b;
+        System.out.println( "the sum is :"+c );
+    }
+}
